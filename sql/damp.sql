eaba77f0bc249fc732bbacc16bb49fc58e27dada
Maks <semenov@mail.ru>
2022-03-13 11:30:11 +0400

    новый damp bd

--- sql/damp.sql
+++ sql/damp.sql
@@ -5,17 +5,6 @@ CREATE TABLE public.theme (
 	CONSTRAINT theme_pk PRIMARY KEY (id),
 	CONSTRAINT theme_un UNIQUE (name)
 );
-
-CREATE TABLE public.var_css_name__theme (
-	id int4 NOT NULL GENERATED ALWAYS AS IDENTITY,
-	id_theme int4 NOT NULL,
-	id_var_css_name int4 NOT NULL,
-	value varchar NULL,
-	CONSTRAINT var_css_name__theme_pk PRIMARY KEY (id),
-	CONSTRAINT var_css_name__theme_fk FOREIGN KEY (id_theme) REFERENCES public.theme(id),
-	CONSTRAINT var_css_name__theme_fk_1 FOREIGN KEY (id_var_css_name) REFERENCES public.var_css_name(id)
-);
-
 CREATE TABLE public.var_css_name (
 	id int4 NOT NULL GENERATED ALWAYS AS IDENTITY,
 	"name" varchar NOT NULL,
@@ -23,3 +12,16 @@ CREATE TABLE public.var_css_name (
 	CONSTRAINT var_css_name_pk PRIMARY KEY (id),
 	CONSTRAINT var_css_name_un UNIQUE (name)
 );
+CREATE TABLE public.var_css_name__theme (
+	id int4 NOT NULL GENERATED ALWAYS AS IDENTITY,
+	id_theme int4 NOT NULL,
+	id_var_css_name int4 NOT NULL,
+	value varchar NULL,
+	CONSTRAINT var_css_name__theme_pk PRIMARY KEY (id),
+	CONSTRAINT var_css_name__theme_un UNIQUE (id_theme, id_var_css_name),
+	CONSTRAINT var_css_name__theme_fk FOREIGN KEY (id_theme) REFERENCES public.theme(id),
+	CONSTRAINT var_css_name__theme_fk_1 FOREIGN KEY (id_var_css_name) REFERENCES public.var_css_name(id)
+);
+
+
+ 
